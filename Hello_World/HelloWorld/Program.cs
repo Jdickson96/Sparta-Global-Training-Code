8db5efbc24ade0115ce3a7b7f20d0f6ca87274d9
Jdickson96 <51116993+Jdickson96@users.noreply.github.com>
2022-02-23 20:34:55 +0000

    Initial Code commit

---
+++ Hello_World/HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿class Wordle
+{
+    static string Guess()
+    {   
+        string guess = "";
+        System.Console.WriteLine("Guess a 5 Letter Word");
+        while (guess.Length != 5)
+        {
+            guess = Console.ReadLine();
+            if (guess.Length < 5)
+            {
+                System.Console.WriteLine("Guess is Too Short");
+            }
+            else if (guess.Length > 5)
+            {
+                System.Console.WriteLine("Guess is Too Long");
+            }
+        }
+        return guess;
+    }
+
+    static string GuessCheck(string guessMade)
+    {
+        string guessBreakdown = "";
+        System.Console.WriteLine("Guess a 5 Letter Word");
+        
+
+        return guessBreakdown;
+    }
+
+    static void Main(string[] args)
+    {
+        string wordleAnswer = "Dodge";    //Answer to seek
+        string guessMade = "";
+        int guessNo = 1;
+
+        guessMade = Guess();
+        guessNo++;
+
+    }
+}
---
+++ Time_Of_Day_Unit_Testing/GreetingsTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+using CodeToTest;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+
+        [TestCase(6)]
+        [TestCase(8)]
+        [Category("Morning during")]
+        public void GivenAtimeBetween5and12Inclusive_Greeting_ReturnsGoodEvening(int time)
+        {
+            Assert.That("Good morning!", Is.EqualTo(Program.Greeting(time)));
+        }
+
+        [TestCase(13)]
+        [TestCase(16)]
+        [Category("Afternoon during")]
+        public void GivenAtimeBetween12and18Inclusive_Greeting_ReturnsGoodAfternoon(int time)
+        {
+            Assert.That("Good afternoon!", Is.EqualTo(Program.Greeting(time)));
+        }
+
+        [TestCase(3)]
+        [TestCase(20)]
+        [Category("Evening during")]
+        public void GivenAtimeBetween18and5Inclusive_Greeting_ReturnsGoodEvening(int time)
+        {
+            Assert.That("Good evening!", Is.EqualTo(Program.Greeting(time)));
+        }
+
+        [Category("Edge case")]
+        [TestCase(5, "Good morning!")]
+        [TestCase(12, "Good morning!")]
+        [TestCase(18, "Good afternoon!")]
+        public void GivenATime_Greeting_ReturnsCorrectMessage(int time, string expected)
+        {
+            Assert.That(expected, Is.EqualTo(Program.Greeting(time)));
+        }
+    }
+}
